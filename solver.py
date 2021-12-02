f852828092921caa1434037bf76eaa6be76a2249
Jonathan Lu <jonathan.lu31@gmail.com>
2021-12-02 14:07:11 -0800

    Fix miscalculation of late benefit

--- solver.py
+++ solver.py
@@ -13,10 +13,11 @@ def greedy(tasks, time):
     viable_tasks = []
     for task in tasks:
         duration = task.get_duration()
-        if time + duration > 1440:
+        end_time = time + duration
+        if end_time > 1440:
             continue
         viable_tasks.append(task)
-        benefit = task.get_late_benefit(time - task.get_deadline())
+        benefit = task.get_late_benefit(end_time - task.get_deadline())
         ratio = benefit / duration
         if ratio >= best_ratio:
             if ratio > best_ratio:
@@ -38,7 +39,7 @@ def greedy(tasks, time):
     if random_float > p:
         rand_task = viable_tasks[random.randint(0, len(viable_tasks) - 1)]
         rand_task_list, rand_profit, rand_remaining = greedy([x for x in tasks if x != rand_task], time + rand_task.get_duration())
-        total_rand_profit = rand_profit + rand_task.get_late_benefit(time - rand_task.get_deadline())
+        total_rand_profit = rand_profit + rand_task.get_late_benefit(time  + rand_task.get_duration() - rand_task.get_deadline())
         if total_rand_profit > profit + best_benefit:
             return [(rand_task, time)] + rand_task_list, total_rand_profit, rand_remaining
     return [(best_task, time)] + task_list, profit + best_benefit, remaining
@@ -52,11 +53,11 @@ def local_search(initial_tasks, soln, remaining):
             task2, timestep2 = soln[i+1]
             rand_index = random.randint(0, len(remaining) - 1)
             local_swap = remaining[rand_index]
-            if local_swap.get_duration() > task.get_duration() + task2.get_duration():
+            local_end = timestep + local_swap.get_duration()
+            if local_end > timestep2 + task2.get_duration():
                 continue
-            local_benefit = local_swap.get_late_benefit(timestep - local_swap.get_deadline())
-            og_benefit = task.get_late_benefit(timestep - task.get_deadline())
-            + task2.get_late_benefit(timestep2 - task2.get_deadline())
+            local_benefit = local_swap.get_late_benefit(local_end - local_swap.get_deadline())
+            og_benefit = task.get_late_benefit(timestep + task.get_duration() - task.get_deadline()) + task2.get_late_benefit(timestep2 + task2.get_duration() - task2.get_deadline())
             if local_benefit > og_benefit:
                 soln.pop(i)
                 soln.pop(i)
@@ -67,7 +68,6 @@ def local_search(initial_tasks, soln, remaining):
                 return local_opt, profit_increase + local_benefit - og_benefit
     return soln, 0
     
-
 def local_search_og(initial_tasks, soln, remaining):
     if not remaining:
         return soln, 0
@@ -83,10 +83,9 @@ def local_search_og(initial_tasks, soln, remaining):
             if local_benefit > og_benefit:
                 soln[i] = (local_swap, timestep)
                 remaining[rand_index] = task
-                local_opt, profit_increase = local_search(initial_tasks, soln, remaining)
+                local_opt, profit_increase = local_search_og(initial_tasks, soln, remaining)
                 return local_opt, profit_increase + local_benefit - og_benefit
     return soln, 0
-
 
 
 def solve(tasks):
@@ -97,7 +96,7 @@ def solve(tasks):
         output: list of igloos in order of polishing  
     """
     output, profit, remaining = greedy(tasks, 0)
-    new_output, increase = local_search(tasks, output, remaining)
+    new_output, increase = local_search_og(tasks, output, remaining)
     print(profit + increase, increase)
     return [task[0].get_task_id() for task in new_output]
         
